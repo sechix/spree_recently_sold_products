76faad4710c201468fe8333b4dc755f5282358a2
sechix <sechix@gmail.com>
2017-06-12 11:48:55 +0200

    error

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -8,8 +8,8 @@ Spree::UsersController.class_eval  do
 
   private
     def load_recently_bought_products
-      @recently_bought_products = @user.orders.order('created_at DESC').collect{|order| order.line_items.collect{|item| item.variant.product } }.flatten
-      @recently_bought_products = @recently_bought_products.uniq
+      # @recently_bought_products = @user.orders.order('created_at DESC').collect{|order| order.line_items.collect{|item| item.variant.product } }.flatten
+      # @recently_bought_products = @recently_bought_products.uniq
     end
 
 
